e6ae4b95191ec67413dceb7e31bbadace55f2b20
Masibulele Madikizela <115924603+DarkSeidXo@users.noreply.github.com>
2023-02-09 11:08:29 +0200

    added firebase

---
+++ config/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app"
+const firebaseConfig = {
+  apiKey: "AIzaSyCYjWBM7ERG40NkhIWacdHRYWSj_jLD4_A",
+  authDomain: "porfolio-2ee3c.firebaseapp.com",
+  projectId: "porfolio-2ee3c",
+  storageBucket: "porfolio-2ee3c.appspot.com",
+  messagingSenderId: "407836044382",
+  appId: "1:407836044382:web:52966ed2b4dc7d80c558cf"
+};
+
+const app = initializeApp(firebaseConfig);
